62baf784eb6b9ab15914b045bb68584288f533cc
DESKTOP-TPEHCNT\spark <sparkkong@gmail.com>
2026-02-25 15:40:44 +0900

    -

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Spax Outpost';
+export const SITE_DESCRIPTION = 'AI Projects & Dev Notes';
