12e99c02394fdd44b695482b4bb01680e770cc29
spax <sparkkong@gmail.com>
2026-03-30 10:10:46 +0900

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.dev-outpost.com',
 	integrations: [mdx(), sitemap()],
 });
